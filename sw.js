8693d55bc8023ceeddc97808a7d43a983bf80d98
thebest861 <102625606+thebest861@users.noreply.github.com>
2026-04-19 22:12:52 -0300

    Refactor service worker with fetch event handling
    Refactor service worker event listeners for clarity and add fetch event handling.

--- sw.js
+++ sw.js
@@ -1,2 +1,13 @@
-self.addEventListener("install", () => self.skipWaiting());
-self.addEventListener("activate", () => self.clients.claim());
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
